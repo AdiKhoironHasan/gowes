fcf69eb7f0a460434d1892caafde64bca0b6d964
adi khoiron hasan <adieron97@gmail.com>
2023-02-08 01:43:25 +0700

    add payload and cons type

--- gowes.go
+++ gowes.go
@@ -3,3 +3,26 @@ package gowes
 func Hello() string {
 	return "Hello World"
 }
+
+type SocketPayload struct {
+	From        string      `json:"from"`
+	To          []Client    `json:"to"`
+	MessageType string      `json:"message_type"`
+	MessageData interface{} `json:"message_data"`
+}
+
+type Client struct {
+	ClientID string `json:"client_id"`
+}
+
+// standard message data
+type Message struct {
+	Title  string `json:"title"`
+	Detail string `json:"detail"`
+}
+
+const (
+	// TypeMessage is a message type
+	TypeGetNotification  = "get_notification"
+	TypeSendNotification = "send_notification"
+)
